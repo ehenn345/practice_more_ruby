72fdbb896e2933320866dcc7edb57858cde2ff1e
Erin <westonerin1@gmail.com>
2021-02-10 14:31:47 -0500

    add double_it method

--- algo_ladder1.rb
+++ algo_ladder1.rb
@@ -26,3 +26,15 @@ end
 
 p lessthan_onehundred9([101, 200, 33, 45, 10])
 
+def double_it(array)
+  i = 0
+  double_array = []
+  while i < array.length
+    double_array << array[i] * 2
+    i += 1
+  end
+  return double_array
+end
+
+p double_it([4, 2, 5, 99, -4])
+
