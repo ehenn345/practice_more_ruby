d3193bb276392e51fa28c560cdca54b79668ae99
Erin <westonerin1@gmail.com>
2021-03-01 15:36:58 -0500

    initial commit for folder, first function problem

---
+++ skills_functions_practice.rb
@@ -0,0 +1,10 @@
+#1 Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+def times_two(number)
+  new_number = number * 2
+  return new_number
+end
+
+p times_two(5)
+
+
